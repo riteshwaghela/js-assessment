ee10b34e453eda13c6992fc49252bda5e3a3a751
Ritesh <riteshwaghela87@gmail.com>
2016-02-15 20:48:33 +0530

    Update functions.js

--- app/functions.js
+++ app/functions.js
@@ -2,23 +2,28 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray : function(fn, arr) {
-
+        fn.apply(null, arr);
   },
 
   speak : function(fn, obj) {
-
+        fn.call(obj);
   },
 
   functionFunction : function(str) {
-
+        return function(anotherStr){
+            return str +','anotherStr;
+        }
   },
 
   makeClosures : function(arr, fn) {
-
+        
   },
 
   partial : function(fn, str1, str2) {
-
+      var params  = Array.prototype.slice.call(arguments, 1);
+      return function() {
+        	return fn.apply(this, params.concat(Array.prototype.slice.call(arguments, 0)));
+      }
   },
 
   useArguments : function() {
@@ -34,6 +39,6 @@ exports.functionsAnswers = {
   },
 
   curryIt : function(fn) {
-
+      
   }
 };
